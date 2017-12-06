776e7ff0c1efb742183749690c301fefb64803a3
dyc <dyc941126@126.com>
2017-12-06 20:13:28 +0800

    bfs and dfs ordering

---
+++ src/edu/cqu/ordering/BFSSyncAgent.java
@@ -0,0 +1,98 @@
+package edu.cqu.ordering;
+
+import edu.cqu.core.Message;
+import edu.cqu.core.SyncAgent;
+import edu.cqu.core.SyncMailer;
+
+import java.util.*;
+
+public abstract class BFSSyncAgent extends SyncAgent {
+
+    private static final int MSG_LAYER = 0XFFFFD0;
+    private static final int MSG_ACK = 0XFFFFD1;
+
+    protected int parent;
+    protected List<Integer> children;
+    protected List<Integer> siblings;
+    protected List<Integer> pseudoChildren;
+    protected List<Integer> pseudoParent;
+    protected int level;
+
+    private Set<Integer> messageReceived;
+
+    public BFSSyncAgent(int id, int[] domain, int[] neighbours, Map<Integer, int[][]> constraintCosts, Map<Integer, int[]> neighbourDomains, SyncMailer mailer) {
+        super(id, domain, neighbours, constraintCosts, neighbourDomains, mailer);
+        children = new LinkedList<>();
+        siblings = new LinkedList<>();
+        pseudoChildren = new LinkedList<>();
+        pseudoParent = new LinkedList<>();
+        messageReceived = new HashSet<>();
+        level = Integer.MAX_VALUE;
+    }
+
+    @Override
+    protected void initRun() {
+        if (id == 1){
+            level = 0;
+            for (int neighbourId : neighbours){
+                children.add(neighbourId);
+            }
+            for (int child : children){
+                sendMessage(new Message(id,child,MSG_LAYER,level));
+            }
+        }
+    }
+
+    @Override
+    public void disposeMessage(Message message) {
+        switch (message.getType()){
+            case MSG_LAYER:
+                messageReceived.add(message.getIdSender());
+                int receivedLevel = (int) message.getValue();
+                if (receivedLevel + 1 < level){
+                    level = receivedLevel + 1;
+                    parent = message.getIdSender();
+                    for (int neighbourId : neighbours){
+                        if (neighbourId != parent){
+                            children.add(neighbourId);
+                        }
+                    }
+                    for (int child : children){
+                        sendMessage(new Message(id,child,MSG_LAYER,level));
+                    }
+                    sendMessage(new Message(id,parent,MSG_ACK,null));
+                }
+                else {
+                    if (receivedLevel == level) {
+                        siblings.add(message.getIdSender());
+                    }
+                    else {
+                        pseudoParent.add(message.getIdSender());
+                    }
+                    children.remove(message.getIdSender());
+                }
+                if (messageReceived.size() == neighbours.length){
+                    assignPseudoChildren();
+                    pseudoTreeCreated();
+                }
+                break;
+            case MSG_ACK:
+                messageReceived.add(message.getIdSender());
+                if (messageReceived.size() == neighbours.length){
+                    assignPseudoChildren();
+                    pseudoTreeCreated();
+                }
+        }
+    }
+
+    protected abstract void pseudoTreeCreated();
+
+    private void assignPseudoChildren(){
+        for (int neighbourId : neighbours){
+            if (neighbourId != parent && !children.contains(neighbourId) && !pseudoParent.contains(neighbourId) && !siblings.contains(neighbourId)){
+                pseudoChildren.add(neighbourId);
+            }
+        }
+    }
+
+}
---
+++ src/edu/cqu/ordering/DFSSyncAgent.java
@@ -0,0 +1,118 @@
+package edu.cqu.ordering;
+
+import edu.cqu.core.Message;
+import edu.cqu.core.SyncAgent;
+import edu.cqu.core.SyncMailer;
+
+import java.util.*;
+
+public abstract class DFSSyncAgent extends SyncAgent{
+
+    private static final int MSG_DEGREE = 0XFFFF0;
+    private static final int MSG_DFS = 0XFFFF1;
+    private static final int MSG_DFS_BACKTRACK = 0XFFFF2;
+
+    protected Map<Integer,Integer> degreeView;
+    protected List<Map.Entry<Integer,Integer>> orderedDegree;
+    protected int parent;
+    protected List<Integer> children;
+    private int currentChildIndex;
+    protected List<Integer> pseudoParents;
+
+    public DFSSyncAgent(int id, int[] domain, int[] neighbours, Map<Integer, int[][]> constraintCosts, Map<Integer, int[]> neighbourDomains, SyncMailer mailer) {
+        super(id, domain, neighbours, constraintCosts, neighbourDomains, mailer);
+        degreeView = new HashMap<>();
+        children = new LinkedList<>();
+        pseudoParents = new LinkedList<>();
+        parent = -1;
+    }
+
+    @Override
+    protected void initRun() {
+        for (int neighbourId : neighbours){
+            sendMessage(new Message(id,neighbourId,MSG_DEGREE,neighbours.length));
+        }
+    }
+
+    @Override
+    public void runFinished() {
+
+    }
+
+    @Override
+    public void disposeMessage(Message message) {
+        switch (message.getType()){
+            case MSG_DEGREE:
+                degreeView.put(message.getIdSender(),(int)message.getValue());
+                if (degreeView.size() == neighbours.length){
+                    orderedDegree = new ArrayList<>(degreeView.entrySet());
+                    orderedDegree.sort(new Comparator<Map.Entry<Integer, Integer>>() {
+                        @Override
+                        public int compare(Map.Entry<Integer, Integer> o1, Map.Entry<Integer, Integer> o2) {
+                            return o2.getValue().compareTo(o1.getValue());
+                        }
+                    });
+                    if (id == 1){
+                        HashSet<Integer> visited = new HashSet<>();
+                        visited.add(id);
+                        parent = -1;
+                        children.add(orderedDegree.get(0).getKey());
+                        sendMessage(new Message(id,orderedDegree.get(0).getKey(),MSG_DFS,visited));
+                    }
+                }
+                break;
+            case MSG_DFS: {
+                HashSet<Integer> visited = (HashSet) message.getValue();
+                visited.add(id);
+                parent = message.getIdSender();
+                int selectedChild = 0;
+                for (int i = 0; i < orderedDegree.size(); i++) {
+                    if (visited.contains(orderedDegree.get(i).getKey())) {
+                        if (orderedDegree.get(i).getKey() != parent){
+                            pseudoParents.add(orderedDegree.get(i).getKey());
+                        }
+                        continue;
+                    }
+                    selectedChild = orderedDegree.get(i).getKey();
+                    currentChildIndex = i;
+                    break;
+                }
+                if (selectedChild != 0) {
+                    children.add(selectedChild);
+                    sendMessage(new Message(id, selectedChild, MSG_DFS, visited));
+                }
+                else {
+                    sendMessage(new Message(id, parent, MSG_DFS_BACKTRACK, visited));
+                }
+                break;
+            }
+            case MSG_DFS_BACKTRACK:
+                HashSet<Integer> visited = (HashSet) message.getValue();
+                int selectedChild = 0;
+                for (int i = currentChildIndex + 1; i < orderedDegree.size(); i++){
+                    if (visited.contains(orderedDegree.get(i).getKey())) {
+                        if (orderedDegree.get(i).getKey() != parent){
+                            pseudoParents.add(orderedDegree.get(i).getKey());
+                        }
+                        continue;
+                    }
+                    selectedChild = orderedDegree.get(i).getKey();
+                    currentChildIndex = i;
+                    break;
+                }
+                if (selectedChild != 0){
+                    children.add(selectedChild);
+                    sendMessage(new Message(id, selectedChild, MSG_DFS, visited));
+                }
+                else {
+                    pseudoTreeCreated();
+                    if (id != 1) {
+                        sendMessage(new Message(id, parent, MSG_DFS_BACKTRACK, visited));
+                    }
+                }
+                break;
+        }
+    }
+
+    protected abstract void pseudoTreeCreated();
+}
