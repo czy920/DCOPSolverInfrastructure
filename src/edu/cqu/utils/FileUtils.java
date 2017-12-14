fb46614a3d0ffd2f75007ace317e4d4a7746180f
dyc941126 <dyc941126@126.com>
2017-12-14 21:21:31 +0800

    add dir create support

--- src/edu/cqu/utils/FileUtils.java
+++ src/edu/cqu/utils/FileUtils.java
@@ -1,10 +1,15 @@
 package edu.cqu.utils;
 
+import java.io.File;
 import java.io.FileOutputStream;
 import java.io.PrintStream;
 
 public class FileUtils {
     public static void writeStringToFile(String content, String filePath){
+        File file = new File(filePath);
+        if (!file.getParentFile().exists()){
+            file.getParentFile().mkdirs();
+        }
          try {
              FileOutputStream fileOutputStream = new FileOutputStream(filePath);
              byte[] bits = content.getBytes();
