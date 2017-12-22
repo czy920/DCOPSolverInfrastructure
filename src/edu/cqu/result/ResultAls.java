5ca9ca40fda37a05452f0e478e85397857e82c3a
dyc <dyc941126@126.com>
2017-12-22 14:42:17 +0800

    trim als result

--- src/edu/cqu/result/ResultAls.java
+++ src/edu/cqu/result/ResultAls.java
@@ -11,9 +11,38 @@ public class ResultAls extends ResultCycle {
     }
 
     public void setBestCostInCycle(double[] bestCostInCycle, int tail) {
-         this.bestCostInCycle = new double[tail];
+        int zeroLength = 0;
+        int infinityLength = 0;
+        for (double cost : bestCostInCycle){
+            if (cost < 1e-5){
+                zeroLength++;
+            }
+            else if (cost > 1e8){
+                infinityLength++;
+            }
+            else {
+                break;
+            }
+        }
+         this.bestCostInCycle = new double[tail - zeroLength - infinityLength];
+        int index = 0;
          for (int i = 0; i < tail; i++){
-             this.bestCostInCycle[i] = bestCostInCycle[i];
+             if (bestCostInCycle[i] < 1e-5 || bestCostInCycle[i] > 1e8){
+                 continue;
+             }
+             this.bestCostInCycle[index++] = bestCostInCycle[i];
          }
+         double[] costInCycle = new double[tail - zeroLength - infinityLength];
+        index = 0;
+        for (int i = 0; i < this.costInCycle.length; i++){
+            if (i <= zeroLength){
+                continue;
+            }
+            if (i > this.costInCycle.length - infinityLength){
+                break;
+            }
+            costInCycle[index++] = this.costInCycle[i];
+        }
+        this.costInCycle = costInCycle;
     }
 }
